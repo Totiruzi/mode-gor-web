c433a4b165bdc1417009d277e3fbe370bfbfad7d
Totiruzi <totiruzi@yahoo.com>
2021-07-26 04:22:08 +0300

    Add config global access to created templates

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	// "goreact/pkg/config"
+	"goreact/pkg/config"
 	"goreact/pkg/handlers"
+	"goreact/pkg/render"
 	"log"
 	"net/http"
 )
@@ -9,9 +12,24 @@ import (
 const portNumber = ":8080"
 
 func main() {
-	http.HandleFunc("/", handlers.Home)
+	var app config.AppConfig
 
-	http.HandleFunc("/about", handlers.About)
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("Can not create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	http.HandleFunc("/", handlers.Repo.Home)
+
+	http.HandleFunc("/about", handlers.Repo.About)
 
 	log.Printf("Starting server on port: %s", portNumber)
 
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,14 +1,35 @@
 package handlers
 
 import (
+	"goreact/pkg/config"
+	"goreact/pkg/render"
 	"net/http"
-	"goreact/pkg/rendler"
 )
 
-func Home(w http.ResponseWriter, r *http.Request) {
-	randler.RenderTemplate(w, "home.page.html")
+// Repo the repository used by the handlers
+var Repo *Repository
+
+// Repository is the repository type
+type Repository struct {
+	App *config.AppConfig
 }
 
-func About(w http.ResponseWriter, r *http.Request) {
-	randler.RenderTemplate(w, "about.page.html")
+// NewRepo Creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "home.page.html")
+}
+
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "about.page.html")
 }
