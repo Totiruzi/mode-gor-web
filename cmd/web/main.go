74d6fb9405f618aaf139047383de09cd44083528
Totiruzi <totiruzi@yahoo.com>
2021-07-27 01:05:43 +0300

    Implement Router with a 3rd party package pat

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -27,11 +27,12 @@ func main() {
 
 	render.NewTemplates(&app)
 
-	http.HandleFunc("/", handlers.Repo.Home)
-
-	http.HandleFunc("/about", handlers.Repo.About)
-
 	log.Printf("Starting server on port: %s", portNumber)
 
-	http.ListenAndServe(portNumber, nil)
+	srv := http.Server {
+		Addr: portNumber,
+		Handler: routes(&app),
+	}
+	err = srv.ListenAndServe()
+	log.Fatal(err)
 }
